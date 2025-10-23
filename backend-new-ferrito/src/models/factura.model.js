5460741405433a1a9120737823446822c1e520dc
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-23 14:42:00 -0500

    Update factura.model.js
    CRUD Models Factura

--- backend-new-ferrito/src/models/factura.model.js
+++ backend-new-ferrito/src/models/factura.model.js
@@ -1,5 +1,6 @@
 const pool = require("../config/db");
 
+//Obtener Factura
 async function ObtenerFactura(){
     const query = `
     SELECT * FROM factura
@@ -10,9 +11,50 @@ async function ObtenerFactura(){
     return rows;
 }
 
+//Crear Factura
+async function CrearFactura({id_factura, usuario_id, fecha_creacion, total, estado}){
+    const query =`
+    INSERT INTO factura (id_factura,usuario_id,fecha_creacion,total,estado)
+    VALUES ($1,$2,$3,$4,$5)
+    RETURNING *;
+    `;
+    const values = [id_factura,usuario_id,fecha_creacion,total,estado];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+    
+}
 
+//Modificar Factura
+async function ModificarFactura({id_factura, usuario_id, fecha_creacion, total, estado}){
+const query = `
+    UPDATE factura
+    SET usuario_id = $1,
+        fecha_creacion = $2,
+        total = $3,
+        estado = $4
+    WHERE id_factura = $5
+    RETURNING *;
+  `;
+    const values = [id_factura, usuario_id, fecha_creacion, total, estado];
+    const { rows } = await pool.query(query, values);
+    return rows[0];
+}
+
+//Eliminar Factura
+async function EliminarFactura(id_factura) {
+  const query = `
+    DELETE FROM factura
+    WHERE id_factura = $1
+    RETURNING *;
+  `;
+  const { rows } = await pool.query(query, [id_factura]);
+  return rows[0];
+}
 
 
 module.exports = {
-    ObtenerFactura
+    ObtenerFactura,
+    CrearFactura,
+    ModificarFactura,
+    EliminarFactura
 }
