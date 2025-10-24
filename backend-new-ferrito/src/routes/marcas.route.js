98f066ff53e93b60ca063f89cba92f9b506e9928
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-23 19:49:02 -0500

    Update marcas.route.js

---
+++ backend-new-ferrito/src/routes/marcas.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const MarcasController = require('../controllers/marcas.controller');
+
+// 🔍 Obtener todas las Marcas
+router.get('/', MarcasController.obtenermarcas);
+
+// 🔍 Obtener Marcas por id
+router.get('/marcas/:id', MarcasController.obtenerMarcasId);
+
+// 🆕 Crear nueva Marcas
+router.post('/', MarcasController.crearmarcas);
+
+// ✏️ Modificar Marcas
+router.put('/:id_marca', MarcasController.modificarMarca);
+
+// ❌ Eliminar Marcas
+router.delete('/:id_marca', MarcasController.eliminarMarca);
+
+module.exports = router;
