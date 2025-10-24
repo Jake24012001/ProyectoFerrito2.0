6d63942e6e237750b7e287aabeb4b5ffbef3caf1
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-23 19:49:08 -0500

    Update roles.route.js

---
+++ backend-new-ferrito/src/routes/roles.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const RolesController = require('../controllers/roles.controller');
+
+// 🔍 Obtener todas las Roles
+router.get('/', RolesController.obtenerRoles);
+
+
+// 🆕 Crear nueva  Roles
+router.post('/', RolesController.crearRoles);
+
+// ✏️ Modificar Roles
+router.put('/:id_rol', RolesController.modificarRoles);
+
+// ❌ Eliminar Roles
+router.delete('/:id_rol', RolesController.eliminarRoles);
+
+module.exports = router;
