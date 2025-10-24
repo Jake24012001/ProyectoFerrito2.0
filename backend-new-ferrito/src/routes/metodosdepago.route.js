eb479ae6742e8b0235bfa5816cdec6c08c91202b
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-23 19:49:04 -0500

    Update metodosdepago.route.js

---
+++ backend-new-ferrito/src/routes/metodosdepago.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const metodosPagoController = require('../controllers/metodosdepago.controller');
+
+// 🔍 Obtener todas las MetodosPagos
+router.get('/', metodosPagoController.obtenermetodosPago);
+
+// 🆕 Crear nueva metodo_pago
+router.post('/', metodosPagoController.crearmetodosPago);
+
+// ✏️ Modificar metodo_pago
+router.put('/:id_metodo_pago', metodosPagoController.modificarmetodosPago);
+
+// ❌ Eliminar metodo_pago
+router.delete('/:id_metodo_pago', metodosPagoController.eliminarmetodosPago);
+
+module.exports = router;
