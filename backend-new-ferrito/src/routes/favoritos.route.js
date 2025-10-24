7acb222d8e3b7966996b24725edba34e48a3b70c
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-23 19:48:55 -0500

    Update favoritos.route.js

---
+++ backend-new-ferrito/src/routes/favoritos.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const favoritoController = require('../controllers/favoritos.controller');
+
+// 🔍 Obtener todas las favorito
+router.get('/', favoritoController.obtenerFavorito);
+
+// 🔍 Obtener favorito por usuario
+router.get('/favorito/:id', favoritoController.obtenerfavoritoPorUsuario);
+
+// 🆕 Crear nueva favorito
+router.post('/', favoritoController.crearfavorito);
+
+// ✏️ Modificar favorito
+router.put('/:id_favorito', favoritoController.modificarFavorito);
+
+// ❌ Eliminar favorito
+router.delete('/:id_favorito', favoritoController.eliminarFavorita);
+
+module.exports = router;
