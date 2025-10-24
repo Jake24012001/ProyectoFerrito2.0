337a57641f7c9bfc949bd1fcd363d5761d65868f
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-23 19:48:57 -0500

    Update historialclientes.route.js

---
+++ backend-new-ferrito/src/routes/historialclientes.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const HClienteController = require('../controllers/historialclientes.controller');
+
+// 🔍 Obtener todas las HCLiente
+router.get('/', HClienteController.obtenerHClientes);
+
+// 🆕 Crear nueva HCLiente
+router.post('/', HClienteController.crearHCliente);
+
+// ✏️ Modificar HCLiente
+router.put('/:id_historial_cliente', HClienteController.modificarHcliente);
+
+// ❌ Eliminar HCLiente
+router.delete('/:id_historial_cliente', HClienteController.eliminarHCliente);
+
+module.exports = router;
