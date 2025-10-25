a701469e49edf41bc62af71d83b075f8345175cd
Jake24012001 <oberinho2401@outlook.com>
2025-10-25 17:40:49 -0500

    cambios provisionales

--- microferrito/src/App.tsx
+++ microferrito/src/App.tsx
@@ -1,17 +1,9 @@
 import ferritoLogo from "./assets/logoferrito.png";
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-    <>
-      <div>
-        <h2>New</h2>
-        <img src={ferritoLogo} className="logo" alt="ferrito" />
-        <h1>OBREROS TRABAJANDO</h1>
-        <h3>Proximamente en Ecuador 2025</h3>
-      </div>
-    </>
-  );
+ return <Navbar />
 }
 
 export default App;
