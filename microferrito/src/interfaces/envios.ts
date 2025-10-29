f5592451445b3e6fb9c652da20c2d0792a12669d
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-28 20:14:25 -0500

    Create envios.ts

---
+++ microferrito/src/interfaces/envios.ts
@@ -0,0 +1,9 @@
+export interface envios {
+  id_envio: number;
+  factura_id: number;
+  direccion_envio: string;
+  estado_envio: string;
+  fecha_envio: string;
+  fecha_estrega_estimada: string;
+  fecha_creacion: string;
+}
