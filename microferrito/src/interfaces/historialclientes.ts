fedcc743dfececa6c8ba7a004878fbfc354c533b
Kimvalla <kimberlyvalladolid5@gmail.com>
2025-10-28 20:14:34 -0500

    Create historialclientes.ts

---
+++ microferrito/src/interfaces/historialclientes.ts
@@ -0,0 +1,7 @@
+export interface historialclientes {
+    id_historial_cliente: number;
+    usuario_id: number;
+    fecha_accion: string;
+    descripcion_accion: string;
+    fecha_creacion: string;
+}
