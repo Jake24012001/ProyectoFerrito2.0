55bbd20eb4a158efec72f23d7fe125be3fcaa91a
Jake24012001 <oberinho2401@outlook.com>
2026-01-02 14:45:31 -0500

    cambios

--- backend-new-ferrito/src/routes/carrito.route.js
+++ backend-new-ferrito/src/routes/carrito.route.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const carritoController = require('../controllers/carrito.controller');
+const carritoController = require("../controllers/carrito.controller");
 
-// Obtener carrito activo por email 
-router.get('/:email', carritoController.obtenerCarritoPorEmail);
+// Obtener carrito activo por email
+router.get("/:email", carritoController.obtenerCarritoPorEmail);
 
 // Obtener carrito activo por id_usuario
-router.get('/usuario/:usuario_id', carritoController.obtenerCarritoUsuario);
+router.get("/usuario/:usuario_id", carritoController.obtenerCarritoUsuario);
 
 // Registrar nuevo carrito
-router.post('/', carritoController.registrarCarrito);
+router.post("/", carritoController.registrarCarrito);
 
 // Modificar carrito existente
-router.put('/:id_carrito', carritoController.modificarCarrito);
+router.put("/:id_carrito", carritoController.modificarCarrito);
 
 // Eliminar carrito
-router.delete('/:id_carrito', carritoController.eliminarCarrito);
+router.delete("/:id_carrito", carritoController.eliminarCarrito);
 
 // Cerrar carrito (actualizar estado a comprado)
-router.patch('/cerrar/:id_carrito', carritoController.cerrarCarrito);
+router.patch("/cerrar/:id_carrito", carritoController.cerrarCarrito);
 
 // Obtener o crear carrito activo por email
-router.get('/orcrear/:email', carritoController.obtenerOCrearCarritoPorEmail);
+router.get("/orcrear/:email", carritoController.obtenerOCrearCarritoPorEmail);
 
-router.post('/agregar', controller.agregarProducto);
-router.put('/cantidad/:id_detalle', controller.actualizarCantidad);
-router.delete('/:id_detalle', controller.eliminarProducto);
+router.post("/agregar", carritoController.agregarProducto);
+router.put("/cantidad/:id_detalle", carritoController.actualizarCantidad);
+router.delete("/:id_detalle", carritoController.eliminarProducto);
 
 module.exports = router;
